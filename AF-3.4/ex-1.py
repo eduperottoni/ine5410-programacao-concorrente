944afe670fd3acf7636e00a625456586d6101214
eduperottoni <edu.perottoni@gmail.com>
2022-11-25 10:58:28 -0300

    Implementação AF-python

---
+++ AF-3.4/ex-1.py
@@ -0,0 +1,52 @@
+from time import sleep
+from random import randint
+from threading import Thread, Lock, Condition
+
+def produtor():
+  global buffer
+  for i in range(10):
+    sleep(randint(0,2))           # fica um tempo produzindo...
+    item = 'item ' + str(i)
+    with lock:
+      if len(buffer) == tam_buffer:
+        print('>>> Buffer cheio. Produtor ira aguardar.')
+        lugar_no_buffer.wait()    # aguarda que haja lugar no buffer
+      buffer.append(item)
+      print('Produzido %s (ha %i itens no buffer)' % (item,len(buffer)))
+      item_no_buffer.notify()
+
+def consumidor():
+  global buffer
+  for i in range(10):
+    with lock:
+      if len(buffer) == 0:
+        print('>>> Buffer vazio. Consumidor ira aguardar.')
+        item_no_buffer.wait()   # aguarda que haja um item para consumir 
+      item = buffer.pop(0)
+      print('Consumido %s (ha %i itens no buffer)' % (item,len(buffer)))
+      lugar_no_buffer.notify()
+    sleep(randint(0,2))         # fica um tempo consumindo...
+
+buffer = []
+tam_buffer = 5
+lock = Lock()
+lugar_no_buffer = Condition(lock)
+item_no_buffer = Condition(lock)
+qtd_produtoras = 2
+qtd_consumidoras = 2
+
+produtoras = []
+for i in range(qtd_produtoras):
+  produtoras.append(Thread(target=produtor))
+  produtoras[i].start()
+
+consumidoras = []
+for i in range(qtd_consumidoras):
+  consumidoras.append(Thread(target=consumidor))
+  consumidoras[i].start()
+
+for i in range(2):
+  produtoras[i].join()
+
+for i in range(2):
+  consumidoras[i].join()
---
+++ AF-3.4/ex-2.py
@@ -0,0 +1,45 @@
+from time import sleep
+from random import randint
+from threading import Thread, Semaphore
+
+def produtor():
+  global buffer
+  for i in range(10):
+    sem_vazio.acquire()
+    sleep(randint(0,2))           # fica um tempo produzindo...
+    item = 'item ' + str(i)
+    # verifica se há lugar no buffer
+    buffer.append(item)
+    print('Produzido %s (ha %i itens no buffer)' % (item,len(buffer)))
+    sem_cheio.release()
+
+def consumidor():
+  global buffer
+  for i in range(10):
+    # aguarda que haja um item para consumir 
+    sem_cheio.acquire()
+    item = buffer.pop(0)
+    print('Consumido %s (ha %i itens no buffer)' % (item,len(buffer)))
+    sleep(randint(0,2))         # fica um tempo consumindo...
+    sem_vazio.release()
+
+buffer = []
+tam_buffer = 3
+
+# cria semáforos
+sem_cheio = Semaphore(0)
+sem_vazio = Semaphore(tam_buffer)
+
+#caso se tenha mais de uma thread, deve-se ter Locks para produtores e consumidores
+
+# Cria a thread produtora e a consumidora.
+produtor = Thread(target=produtor) 
+consumidor = Thread(target=consumidor)
+
+# Inicia a thread produtora e a consumidora.
+produtor.start()
+consumidor.start()
+
+# Aguarda a thread produtora e a consumidora finalizarem.
+produtor.join()
+consumidor.join()
